7ffe177fb364161e1095b55f2a0ef58a968503b2
gPagio <guiguipagio@gmail.com>
2023-08-15 20:58:47 -0300

    validacao nos operadores digitados

--- 4ª Semestre/Andre Bessa/si-multivix/app-calculadora/Program.cs
+++ 4ª Semestre/Andre Bessa/si-multivix/app-calculadora/Program.cs
@@ -7,12 +7,15 @@ internal class Program
     {
         double valor1;
         double valor2;
+        var opcao = 0;
 
         Console.WriteLine("Insira Valor 1: ");
         valor1 = double.Parse(Console.ReadLine()!);
+        Console.WriteLine("");
 
         Console.WriteLine("Insira Valor 2: ");
         valor2 = double.Parse(Console.ReadLine()!);
+        Console.WriteLine("");
 
 
         Console.WriteLine("Escolha a operação:");
@@ -20,29 +23,38 @@ internal class Program
         Console.WriteLine("[-] Subtração");
         Console.WriteLine("[*] Multiplicação");
         Console.WriteLine("[/] Divisão");
-        Console.WriteLine("Digite a opção: ");
-        var opcao = Console.ReadLine()![0];
+
+        while(opcao != '+' && opcao != '-' && opcao != '*' && opcao != '/')
+        {
+            Console.WriteLine("Digite a opção: ");
+            opcao = Console.ReadLine()![0];
+            Console.WriteLine("");
+        }
 
         switch (opcao)
         {
             case '+':
                 double _soma = Soma(valor1, valor2);
                 Console.WriteLine("A soma é: " + _soma);
+                Console.WriteLine("");
                 break;
 
             case '-':
                 double _sub = Subtracao(valor1, valor2);
                 Console.WriteLine("A subtracao é: " + _sub);
+                Console.WriteLine("");
                 break;
 
             case '*':
                 double _mul = Multiplicacao(valor1, valor2);
                 Console.WriteLine("A multiplicacao é: " + _mul);
+                Console.WriteLine("");
                 break;
 
             case '/':
                 double _div = Divisao(valor1, valor2);
                 Console.WriteLine("A divisao é: " + _div);
+                Console.WriteLine("");
                 break;
         }
     }
