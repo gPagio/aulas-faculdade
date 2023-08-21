f87b8114f667f866bd5d6b5ad1354413c4e4b486
gPagio <116508581+gPagio@users.noreply.github.com>
2023-08-21 17:35:01 -0300

    feat: inicio construcao menu principal

---
+++ 4ª Semestre/Andre Bessa/Atividades/2023-08-15/MostraHoraComputador.cs
@@ -0,0 +1,8 @@
+
+public class MostraHoraComputador{
+    public static void Main(String[] args) {
+        DateTime dataAtual = DateTime.Now;
+        
+        Console.WriteLine(dataAtual.ToString());
+    }
+}
