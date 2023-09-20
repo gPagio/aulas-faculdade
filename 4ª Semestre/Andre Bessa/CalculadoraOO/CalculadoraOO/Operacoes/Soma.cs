0c1f1da53e2dff318cde9b9ede7366fc5aca9a6d
gPagio <guiguipagio@gmail.com>
2023-09-20 20:56:35 -0300

    feat: inicio classe main

---
+++ 4ª Semestre/Andre Bessa/CalculadoraOO/CalculadoraOO/Operacoes/Soma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraOO.Operacoes
+{
+    internal class Soma : Operacao
+    {
+        public Soma(double valor1, double valor2) : base(valor1, valor2)
+        {
+        }
+
+        public override double Calcular(double valor1, double valor2)
+        {
+            return valor1 + valor2;
+        }
+    }
+}
--- 4ª Semestre/Andre Bessa/CalculadoraOO/CalculadoraOO/Program.cs
+++ 4ª Semestre/Andre Bessa/CalculadoraOO/CalculadoraOO/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraOO
+{
+    internal class Program
+    {
+        public static double valor1;
+        public static double valor2;
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("CALCULADORA");
+            Console.WriteLine("");
+
+            Console.WriteLine("Informe 1º Num:");
+            valor1 = double.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Informe 2º Num:");
+            valor2 = double.Parse(Console.ReadLine()!);
+        }
+    }
+}
